92680e875a5926fd1b60b668b198ee239c3a38ec
hugoon <935497789@qq.com>
2022-06-01 21:03:39 +0800

    fix file lost

--- build/web-desktop/src/chunks/bundle.js
+++ build/web-desktop/src/chunks/bundle.js
@@ -19289,134 +19289,6 @@ System.register("chunks:///_virtual/index5.js", ['./cjs-loader.mjs'], function (
 
       var _cjsExports;
 
-      var __cjsMetaURL = exports('__cjsMetaURL', module.meta.url);
-
-      loader.define(__cjsMetaURL, function (exports$1, require, module, __filename, __dirname) {
-        /**
-         * A minimal UTF8 implementation for number arrays.
-         * @memberof util
-         * @namespace
-         */
-        var utf8 = exports$1;
-        /**
-         * Calculates the UTF8 byte length of a string.
-         * @param {string} string String
-         * @returns {number} Byte length
-         */
-
-        utf8.length = function utf8_length(string) {
-          var len = 0,
-              c = 0;
-
-          for (var i = 0; i < string.length; ++i) {
-            c = string.charCodeAt(i);
-            if (c < 128) len += 1;else if (c < 2048) len += 2;else if ((c & 0xFC00) === 0xD800 && (string.charCodeAt(i + 1) & 0xFC00) === 0xDC00) {
-              ++i;
-              len += 4;
-            } else len += 3;
-          }
-
-          return len;
-        };
-        /**
-         * Reads UTF8 bytes as a string.
-         * @param {Uint8Array} buffer Source buffer
-         * @param {number} start Source start
-         * @param {number} end Source end
-         * @returns {string} String read
-         */
-
-
-        utf8.read = function utf8_read(buffer, start, end) {
-          var len = end - start;
-          if (len < 1) return "";
-          var parts = null,
-              chunk = [],
-              i = 0,
-              // char offset
-          t; // temporary
-
-          while (start < end) {
-            t = buffer[start++];
-            if (t < 128) chunk[i++] = t;else if (t > 191 && t < 224) chunk[i++] = (t & 31) << 6 | buffer[start++] & 63;else if (t > 239 && t < 365) {
-              t = ((t & 7) << 18 | (buffer[start++] & 63) << 12 | (buffer[start++] & 63) << 6 | buffer[start++] & 63) - 0x10000;
-              chunk[i++] = 0xD800 + (t >> 10);
-              chunk[i++] = 0xDC00 + (t & 1023);
-            } else chunk[i++] = (t & 15) << 12 | (buffer[start++] & 63) << 6 | buffer[start++] & 63;
-
-            if (i > 8191) {
-              (parts || (parts = [])).push(String.fromCharCode.apply(String, chunk));
-              i = 0;
-            }
-          }
-
-          if (parts) {
-            if (i) parts.push(String.fromCharCode.apply(String, chunk.slice(0, i)));
-            return parts.join("");
-          }
-
-          return String.fromCharCode.apply(String, chunk.slice(0, i));
-        };
-        /**
-         * Writes a string as UTF8 bytes.
-         * @param {string} string Source string
-         * @param {Uint8Array} buffer Destination buffer
-         * @param {number} offset Destination offset
-         * @returns {number} Bytes written
-         */
-
-
-        utf8.write = function utf8_write(string, buffer, offset) {
-          var start = offset,
-              c1,
-              // character 1
-          c2; // character 2
-
-          for (var i = 0; i < string.length; ++i) {
-            c1 = string.charCodeAt(i);
-
-            if (c1 < 128) {
-              buffer[offset++] = c1;
-            } else if (c1 < 2048) {
-              buffer[offset++] = c1 >> 6 | 192;
-              buffer[offset++] = c1 & 63 | 128;
-            } else if ((c1 & 0xFC00) === 0xD800 && ((c2 = string.charCodeAt(i + 1)) & 0xFC00) === 0xDC00) {
-              c1 = 0x10000 + ((c1 & 0x03FF) << 10) + (c2 & 0x03FF);
-              ++i;
-              buffer[offset++] = c1 >> 18 | 240;
-              buffer[offset++] = c1 >> 12 & 63 | 128;
-              buffer[offset++] = c1 >> 6 & 63 | 128;
-              buffer[offset++] = c1 & 63 | 128;
-            } else {
-              buffer[offset++] = c1 >> 12 | 224;
-              buffer[offset++] = c1 >> 6 & 63 | 128;
-              buffer[offset++] = c1 & 63 | 128;
-            }
-          }
-
-          return offset - start;
-        }; // #endregion ORIGINAL CODE
-
-
-        _cjsExports = exports('default', module.exports);
-      }, {});
-    }
-  };
-});
-
-System.register("chunks:///_virtual/index6.js", ['./cjs-loader.mjs'], function (exports, module) {
-  'use strict';
-
-  var loader;
-  return {
-    setters: [function (module) {
-      loader = module.default;
-    }],
-    execute: function () {
-      exports('default', void 0);
-
-      var _cjsExports;
-
       var _writeFloatLE;
 
       var _writeFloatBE;
@@ -19765,6 +19637,134 @@ System.register("chunks:///_virtual/index6.js", ['./cjs-loader.mjs'], function (
   };
 });
 
+System.register("chunks:///_virtual/index6.js", ['./cjs-loader.mjs'], function (exports, module) {
+  'use strict';
+
+  var loader;
+  return {
+    setters: [function (module) {
+      loader = module.default;
+    }],
+    execute: function () {
+      exports('default', void 0);
+
+      var _cjsExports;
+
+      var __cjsMetaURL = exports('__cjsMetaURL', module.meta.url);
+
+      loader.define(__cjsMetaURL, function (exports$1, require, module, __filename, __dirname) {
+        /**
+         * A minimal UTF8 implementation for number arrays.
+         * @memberof util
+         * @namespace
+         */
+        var utf8 = exports$1;
+        /**
+         * Calculates the UTF8 byte length of a string.
+         * @param {string} string String
+         * @returns {number} Byte length
+         */
+
+        utf8.length = function utf8_length(string) {
+          var len = 0,
+              c = 0;
+
+          for (var i = 0; i < string.length; ++i) {
+            c = string.charCodeAt(i);
+            if (c < 128) len += 1;else if (c < 2048) len += 2;else if ((c & 0xFC00) === 0xD800 && (string.charCodeAt(i + 1) & 0xFC00) === 0xDC00) {
+              ++i;
+              len += 4;
+            } else len += 3;
+          }
+
+          return len;
+        };
+        /**
+         * Reads UTF8 bytes as a string.
+         * @param {Uint8Array} buffer Source buffer
+         * @param {number} start Source start
+         * @param {number} end Source end
+         * @returns {string} String read
+         */
+
+
+        utf8.read = function utf8_read(buffer, start, end) {
+          var len = end - start;
+          if (len < 1) return "";
+          var parts = null,
+              chunk = [],
+              i = 0,
+              // char offset
+          t; // temporary
+
+          while (start < end) {
+            t = buffer[start++];
+            if (t < 128) chunk[i++] = t;else if (t > 191 && t < 224) chunk[i++] = (t & 31) << 6 | buffer[start++] & 63;else if (t > 239 && t < 365) {
+              t = ((t & 7) << 18 | (buffer[start++] & 63) << 12 | (buffer[start++] & 63) << 6 | buffer[start++] & 63) - 0x10000;
+              chunk[i++] = 0xD800 + (t >> 10);
+              chunk[i++] = 0xDC00 + (t & 1023);
+            } else chunk[i++] = (t & 15) << 12 | (buffer[start++] & 63) << 6 | buffer[start++] & 63;
+
+            if (i > 8191) {
+              (parts || (parts = [])).push(String.fromCharCode.apply(String, chunk));
+              i = 0;
+            }
+          }
+
+          if (parts) {
+            if (i) parts.push(String.fromCharCode.apply(String, chunk.slice(0, i)));
+            return parts.join("");
+          }
+
+          return String.fromCharCode.apply(String, chunk.slice(0, i));
+        };
+        /**
+         * Writes a string as UTF8 bytes.
+         * @param {string} string Source string
+         * @param {Uint8Array} buffer Destination buffer
+         * @param {number} offset Destination offset
+         * @returns {number} Bytes written
+         */
+
+
+        utf8.write = function utf8_write(string, buffer, offset) {
+          var start = offset,
+              c1,
+              // character 1
+          c2; // character 2
+
+          for (var i = 0; i < string.length; ++i) {
+            c1 = string.charCodeAt(i);
+
+            if (c1 < 128) {
+              buffer[offset++] = c1;
+            } else if (c1 < 2048) {
+              buffer[offset++] = c1 >> 6 | 192;
+              buffer[offset++] = c1 & 63 | 128;
+            } else if ((c1 & 0xFC00) === 0xD800 && ((c2 = string.charCodeAt(i + 1)) & 0xFC00) === 0xDC00) {
+              c1 = 0x10000 + ((c1 & 0x03FF) << 10) + (c2 & 0x03FF);
+              ++i;
+              buffer[offset++] = c1 >> 18 | 240;
+              buffer[offset++] = c1 >> 12 & 63 | 128;
+              buffer[offset++] = c1 >> 6 & 63 | 128;
+              buffer[offset++] = c1 & 63 | 128;
+            } else {
+              buffer[offset++] = c1 >> 12 | 224;
+              buffer[offset++] = c1 >> 6 & 63 | 128;
+              buffer[offset++] = c1 & 63 | 128;
+            }
+          }
+
+          return offset - start;
+        }; // #endregion ORIGINAL CODE
+
+
+        _cjsExports = exports('default', module.exports);
+      }, {});
+    }
+  };
+});
+
 System.register("chunks:///_virtual/index7.js", ['./cjs-loader.mjs'], function (exports, module) {
   'use strict';
 
@@ -20088,7 +20088,7 @@ System.register("chunks:///_virtual/minimal.js", ['./cjs-loader.mjs', './index-m
   };
 });
 
-System.register("chunks:///_virtual/minimal2.js", ['./cjs-loader.mjs', './index.js', './index2.js', './index4.js', './index6.js', './index3.js', './index5.js', './index7.js', './longbits.js'], function (exports, module) {
+System.register("chunks:///_virtual/minimal2.js", ['./cjs-loader.mjs', './index.js', './index2.js', './index4.js', './index5.js', './index3.js', './index6.js', './index7.js', './longbits.js'], function (exports, module) {
   'use strict';
 
   var loader, __cjsMetaURL$1, __cjsMetaURL$2, __cjsMetaURL$3, __cjsMetaURL$4, __cjsMetaURL$5, __cjsMetaURL$6, __cjsMetaURL$7, __cjsMetaURL$8;
